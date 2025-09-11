8cafd24377ea49a2a30d428992d0068867f935c0
sportsportalgndec <sportsportalgndec@gmail.com>
2025-09-11 17:11:50 +0530

    Update CreateCaptain.jsx

--- src/pages/CreateCaptain.jsx
+++ src/pages/CreateCaptain.jsx
@@ -54,7 +54,7 @@ export default function CreateCaptain() {
   ];
 
   const years = [1, 2, 3, 4, 5];
-  const teamSizes = [1,2,3,4,5, 6, 7, 8, 9, 10];
+  const teamSizes = [1,2,3,4,5, 6, 7, 8, 9, 10,11,12,13,14];
   const sportsList = [
     "Football",
     "Basketball",
