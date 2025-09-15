15069c1185fec6e21b8295ceb4ea367d28f6a6a2
sportsportalgndec <sportsportalgndec@gmail.com>
2025-09-15 19:49:36 +0530

    Update options.js

--- src/lib/options.js
+++ src/lib/options.js
@@ -24,7 +24,7 @@ export const branches = [
   "BBA (Bachelor of Business Administration)",
   "B.Voc.(Interior Design)",
   "B.Com.(Entrepreneurship)",
-  "B. Arch"
+  "B.Arch (Bachelor of Architecture)"
 ];
 
 export const years = [1, 2, 3, 4, 5];
