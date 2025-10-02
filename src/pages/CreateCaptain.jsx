01790dc01727156b6cbbbacc60033a2d78b83afb
sportsportalgndec <sportsportalgndec@gmail.com>
2025-10-02 14:16:56 +0530

    Update CreateCaptain.jsx

--- src/pages/CreateCaptain.jsx
+++ src/pages/CreateCaptain.jsx
@@ -34,7 +34,7 @@ export default function CreateCaptain() {
   // Form arrays (reused from shared options)
   const branches = sharedBranches;
   const years = sharedYears;
-  const teamSizes = [1,2,3,4,5, 6, 7, 8, 9, 10];
+  const teamSizes = [1,2,3,4,5, 6, 7, 8, 9, 10,11,12,13,14,15];
   const sportsList = sharedSports;
 
   const [form, setForm] = useState({
